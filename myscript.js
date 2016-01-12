507b1bcf0187f684f5b2250884d93c56e3a1e05d
FumikoIshizawa <p.krrr.731@gmail.com>
2016-01-12 20:13:36 +0900

    Post json data to api Desing with material design lite

--- myscript.js
+++ myscript.js
@@ -7,6 +7,24 @@ chrome.tabs.query({active: true}, function(tabs) {
 	console.log(tab);
 	chrome.tabs.get(tab.id, function(tab) {
 		document.getElementById("recipe_title").value = tab.title;
-		document.getElementById("recipe_url").innerHTML = tab.url;
+		document.getElementById("recipe_url").value = tab.url;
+	});
+});
+
+$(function() {
+	$('#execute').click(function() {
+		$.ajax({
+			url: 'http://127.0.0.1:8000/api/v1/recipe/',
+			type: 'post',
+			dataType: 'json',
+			data: {
+				title: $('#recipe_title').val(),
+				url: $('#recipe_url').val(),
+				category: 0
+			},
+			success: function(data) {
+				alert(data);
+			}
+		});
 	});
 });
